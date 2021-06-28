bf4b391685f66ae91198dd718fc424fb26cc18ab
PaulWebber <paulw@tehnerdz.com>
2021-06-28 16:27:20 -0600

    new git

--- OMDbCall.py
+++ OMDbCall.py
@@ -1,15 +1,19 @@
 import omdb
 import json
 import datetime
+import os
 
 #set the API key
-omdb.set_default('apikey', 'API_KEY_HERE')
+omdb.set_default('apikey', 'key')
 #response = the search criteria results
-response = omdb.get(title='The Lighthouse', year=2019, fullplot=True, tomatoes=True)
+response = omdb.get(title='Little Miss Sunshine', year=2006, fullplot=True, tomatoes=True)
 #convert json response to python object
 resp = json.loads(json.dumps(response))
+#folder to put ckad.md in
+ckadPath = os.path.join("path","readme.md")
 
-with open("ckad.md","a") as ckad:
+
+with open(ckadPath,"w") as ckad:
     print("+++\nauthor = \"Paul Webber\"\ntitle = \"" + resp['title']+"\"\ndate = \""\
           +datetime.datetime.now().strftime("%Y-%m-%d")+"\"" \
           "\ndescription = \"Screening June 25, 2021\"\n+++\n", file=ckad)
